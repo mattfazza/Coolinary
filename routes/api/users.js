a17f5ccb925b282514a45bcebe3363bd499eced3
mattfazza <mathewsfazza@gmail.com>
2019-01-13 14:57:00 -0600

    added an endpoint to register users

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+
+
+//load user model
+const User = require('../../models/User');
 
 //since we've already defined use in the server, we can just use /test as an endpoint
 //we'll actually input /api/users/test to test this
@@ -8,5 +14,42 @@ const router = express.Router();
 // @desc Tests users route
 // @access Public
 router.get('/test', (req, res) => res.json({ msg: "Users Works" }));
+
+// @route   GET api/posts/register
+// @desc    Register user
+// @access  Public
+router.post('/register', (req, res) => {
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if (user) {
+                return res.status(400).json({ email: 'Email already exists' })
+            } else {
+                const avatar = gravatar.url(req.body.email, {
+                    s: '200', //size
+                    r: 'pg', //rating
+                    d: 'mm' //default
+                });
+
+                const newUser = new User({
+                    name: req.body.name,
+                    email: req.body.email,
+                    avatar,
+                    password: req.body.password
+                });
+
+
+                //we're are providin a user, an email, and a password and that user is sent back to us in the response
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        newUser.password = hash;
+                        newUser.save()
+                            .then(user => res.json(user))
+                            .catch(err => console.log(err));
+                    });
+                });
+            }
+        });
+
+});
 
 module.exports = router;
