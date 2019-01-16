b29f5f585191d771fd311c1cabe2dda8f38cadd6
mattfazza <mathewsfazza@gmail.com>
2019-01-15 22:32:25 -0600

    Worked on some more validation

--- validation/register.js
+++ validation/register.js
@@ -5,8 +5,17 @@ module.exports = function validateRegisterInput(data) {
 
     let errors = {};
 
+    //if it's not empty, it is whatever it is which is data.name.  If it is empty, then it's gonna be an empty string
+
+    data.name = !isEmpty(data.name) ? data.name : '';
+
+
     if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
         errors.name = 'Name must be between 2 and 30 characters';
+    }
+
+    if (Validator.isEmpty(data.name)) {
+        errors.name = 'Name field is required';
     }
 
     return {
