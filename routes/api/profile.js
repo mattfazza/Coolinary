af893de3b7fcd1e8ff3803899837817f56397919
mattfazza <mathewsfazza@gmail.com>
2019-01-18 20:52:50 -0600

    Updated profile.js to make sure users without a profile "get caught"

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+//  Load Profile Model and User model
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
 
 //since we've already defined use in the server, we can just use /test as an endpoint
 
@@ -7,5 +14,22 @@ const router = express.Router();
 // @desc Tests profile route
 // @access Public
 router.get('/test', (req, res) => res.json({ msg: "Posts Works" }));
+
+//  @route  GET api/profile
+//  @desc   Get current user
+//  @access Private
+router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+    const errors = {};
+
+    Profile.findOne({ user: req.user.id })
+        .then(profile => {
+            if (!profile) {
+                errors.noprofile = 'Theres no profile for this user';
+                return res.status(404).json(errors);
+            }
+            res.json(profile);
+        })
+        .catch(err => res.status(404).json(err));
+});
 
 module.exports = router;
