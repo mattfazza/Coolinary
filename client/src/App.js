b122969f61d006687c73e005aa339b6ca9741902
mattfazza <mathewsfazza@gmail.com>
2019-02-10 19:04:25 -0600

    Added BrowserRouter and fixed import problems

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
@@ -7,14 +8,19 @@ import Landing from './components/layout/Landing';
 import './App.css';
 
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        <h1>My React App</h1>
-        <Navbar />
-        <Landing />
-        <Footer />
+      <Router>
+        <div className="App">
+          <h1>My React App</h1>
+          <Navbar />
+          <Route exact path="/" component={ Landing } />
+          <Footer />
       </div>
+      </Router>
     );
   }
 }
+
+export default App;
