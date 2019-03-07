04d3c91aa6268bd33f16e076db219995349d4e2c
mattfazza <mathewsfazza@gmail.com>
2019-03-06 18:47:28 -0600

    Moving axios to authActions

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import axios from 'axios';
 import classnames from 'classnames';
 import { connect } from 'react-redux';
 import { registerUser } from '../../actions/authActions';
